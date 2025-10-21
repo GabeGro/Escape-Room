26d6f5187936e4741302de2787da2540b3ad4661
GabeGro <ggroenwo@ucsc.edu>
2025-10-21 13:27:28 -0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 600,
+    height: 800,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    scene: [ Menu , Play ]
+}
+
+let game = new Phaser.Game(config)
+
+let { width, height } = game.config
